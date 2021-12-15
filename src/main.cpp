f2c73332ccd0c0997679d891c24c0d5837087d36
Fynn Raddatz <fynn.raddatz@gmail.com>
2021-12-15 13:41:52 +0100

    ROS 2 lol

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include "PCB.hpp"
 #include <unistd.h>
 #include <thread>
-#include "std_msgs/String.h"
-#include "ros/ros.h"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
 
 PCB *pcbHandle;
 
@@ -12,13 +14,14 @@ int main(int argc, char const *argv[])
    UART uart = UART("/dev/ttyUSB0");
    PCB pcb = PCB(&uart);
    pcbHandle = &pcb;
-
-   ros::init(argc, argv, "pcb_talker");
+   /*
+   rclcpp::init(argc, argv, "pcb_talker");
    ros::NodeHandle ros_handle;
    ros::Publisher pubULTRA = ros_handle.advertise<std_msgs::String>("pcb/ultra", 100);
    ros::Publisher pubBAT = ros_handle.advertise<std_msgs::String>("pcb/battery", 100);
    ros::Publisher pubTEMP = ros_handle.advertise<std_msgs::String>("pcb/temperatures", 100);
    ros::Subscriber subscriber = ros_handle.subscribe("pcb/control", 100, controlCallback);
+   */
 
    usleep(100000);
    pcb.setMode(ENABLE);
@@ -30,8 +33,11 @@ int main(int argc, char const *argv[])
    pcb.setMode(SELFCHECK);
    usleep(100000);
 
+   MinimalPublisher* batPub = std::make_shared<MinimalPublisher>();
+
    while (true)
    {
+      /*
       PUBLISH_MESSAGE updateMessage = pcb.receiveAndParse();
 
       if (updateMessage == UPDATE_NONE)
@@ -66,6 +72,11 @@ int main(int argc, char const *argv[])
          printf("UART ROS: Published Temperature status\n");
          break;
       }
+      */
+
+     batPub->publishMessage("TEST!");
+      rclcpp::spin_once();   
+      usleep(100000);
    }
 }
 
@@ -88,3 +99,28 @@ void controlCallback(const std_msgs::String::ConstPtr &msg)
       break;
    }
 }
+
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+   MinimalPublisher() : Node("minimal_publisher"), count_(0)
+   {
+      publisher_ = this->create_publisher<std_msgs::msg::String>("pcb/battery", 10);
+   }
+   
+   void publishMessage(std::string msg) 
+   {
+      auto message = std_msgs::msg::String();
+      message.data = msg;
+      publisher_->publish(message);
+      printf("UART ROS: Published message!");
+   }
+
+   
+  private:
+   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+   rclcpp::TimerBase::SharedPtr timer_;
+    
+   size_t count_;
+};
